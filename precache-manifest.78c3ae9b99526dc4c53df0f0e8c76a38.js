7f9d2729e8abca791ed562403abae28acd262419
John Michael Del Mundo <jmdelmundo10@yahoo.com>
2020-06-18 13:48:47 +0800

    Updates

---
+++ precache-manifest.78c3ae9b99526dc4c53df0f0e8c76a38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b7ee8d7f15d64f0e68e501c81132f3a",
+    "url": "/ReactEncouragement/index.html"
+  },
+  {
+    "revision": "292d9e47212bcc9368b5",
+    "url": "/ReactEncouragement/static/css/main.3da68c18.chunk.css"
+  },
+  {
+    "revision": "d6f12d9879811dc8f42e",
+    "url": "/ReactEncouragement/static/js/2.7f890dda.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactEncouragement/static/js/2.7f890dda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "292d9e47212bcc9368b5",
+    "url": "/ReactEncouragement/static/js/main.b87d9174.chunk.js"
+  },
+  {
+    "revision": "749a5438a646b711c8e3",
+    "url": "/ReactEncouragement/static/js/runtime-main.111b0d50.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactEncouragement/static/media/logo.5d5d9eef.svg"
+  }
+]);
